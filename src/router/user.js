adfb1f57b725569d72fcae2ff2d912e3b2537955
Benoit-Welsch <bwelsch@helb-prigogine.be>
2020-10-22 09:55:08 +0200

    user route

--- src/router/user.js
+++ src/router/user.js
@@ -1,14 +1,19 @@
 const router = require('express').Router();
 const Controller = require('../controller/controller');
-const {userBodyChecker} = require('../middleware/bodyChecker');
+const { userBodyChecker } = require('../middleware/bodyChecker');
+const { auth, role } = require('../middleware/auth');
 const user = new Controller('User');
 
 router.get('/', user.get);
 
 router.post('/', userBodyChecker, user.create);
 
-router.patch('/:id', user.updateById);
+router.patch('/me', auth, user.deleteMe);
 
-router.delete('/:id', user.deleteById);
+router.patch('/:id',auth, role('admin'), user.updateById);
+
+router.delete('/me', auth, user.deleteMe);
+
+router.delete('/:id', auth, role('admin'), user.deleteById);
 
 module.exports = router;
