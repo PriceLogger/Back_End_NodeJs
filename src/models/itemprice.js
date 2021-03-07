ce52ae13101fcff98a24f2cff9b6eab9f2ffb102
Benoit Welsch <bwelsch@helb-prigogine.be>
2021-03-07 18:30:49 +0100

    Route to save fetched price in DB

--- src/models/itemprice.js
+++ src/models/itemprice.js
@@ -1,28 +1,38 @@
-const {Model} = require('sequelize');
+const { Model } = require('sequelize');
+const HttpError = require('../error/httpError');
 
 module.exports = (sequelize, DataTypes) => {
-  class ItemPrice extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      models.ItemPrice.belongsTo(models.Item, {
-        foreignKey: {
-          name: 'ItemId',
-          allowNull: false
+    class ItemPrice extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            models.ItemPrice.belongsTo(models.Item, {
+                foreignKey: {
+                    name: 'ItemId',
+                    allowNull: false
+                }
+            });
         }
-      });
+    };
+    ItemPrice.init({
+        ItemId: DataTypes.INTEGER,
+        price: DataTypes.STRING,
+    }, {
+        sequelize,
+        modelName: 'ItemPrice',
+    });
+
+    ItemPrice.ingest = async function(prices) {
+        if (!Array.isArray(prices)) throw new HttpError('Invalid itemPrice', 404, { reason: 'not a array' })
+        prices.forEach(price => {
+            if (!price.ItemId || !price.price) throw new HttpError('Invalid itemPrice', 404, { reason: 'missing price or itemId on one of the value' })
+        });
+        return await ItemPrice.bulkCreate(prices);
     }
-  };
-  ItemPrice.init({
-    ItemId: DataTypes.INTEGER,
-    price: DataTypes.STRING,
-  }, {
-    sequelize,
-    modelName: 'ItemPrice',
-  });
-  return ItemPrice;
+
+    return ItemPrice;
 };
--- src/router/item.js
+++ src/router/item.js
@@ -1,24 +1,31 @@
 const router = require('express').Router();
 const Item = require('../models/').Item;
 const ItemPrice = require('../models/').ItemPrice;
-const {check} = require('../middleware/validator');
-const {role} = require('../middleware/auth');
+const { check, hasBody } = require('../middleware/validator');
 
-router.get('/', async ({res}) => {
-  res.json(await Item.findAll({include: ItemPrice}));
+router.get('/', async({ res }) => {
+    res.json(await Item.findAll({ include: ItemPrice }));
 });
 
-router.get('/:id(\\d+)/', async (req, res) => {
-  res.json(await Item.findByPk(req.params.id, {include: ItemPrice}));
+router.get('/:id(\\d+)/', async(req, res) => {
+    res.json(await Item.findByPk(req.params.id, { include: ItemPrice }));
 });
 
-router.post('/', check(['url']), async (req, res, next) => {
-  try {
-    res.json(await Item.new(req.body.url))
-  } catch (err) {
-    next(err);
-  }
+router.post('/', check(['url']), async(req, res, next) => {
+    try {
+        res.json(await Item.new(req.body.url))
+    } catch (err) {
+        next(err);
+    }
 });
+
+router.post('/price', hasBody(), async(req, res, next) => {
+    try {
+        res.json(await ItemPrice.ingest(req.body));
+    } catch (err) {
+        next(err);
+    }
+})
 
 /*router.patch('/:id', role('admin'), item.updateById);
 
