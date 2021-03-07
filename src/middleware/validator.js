bfcbb15024686b3f56dd7b5b166a091ad1c18b7e
Benoit Welsch <bwelsch@helb-prigogine.be>
2021-03-07 18:30:25 +0100

    hasBody function fixed in Validator (middleware) :  - the hasBody was missing a next when condition was met  - the condition was always return true

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,36 +1,40 @@
 const HttpError = require("../error/httpError");
 
 const check = (field) => {
-  return (req, res, next) => {
-    let body = req.body;
-    let correct = true;
-    let missing = [];
-    field.forEach(key => {
-      if (!body.hasOwnProperty(key)) {
-        missing.push(key);
-        correct = false;
-      }
-    });
-    if (correct) {
-      next();
-    } else {
-      next(new HttpError('Some required fields are not filled in', 400, {missing: missing}))
+    return (req, res, next) => {
+        let body = req.body;
+        let correct = true;
+        let missing = [];
+        field.forEach(key => {
+            if (!body.hasOwnProperty(key)) {
+                missing.push(key);
+                correct = false;
+            }
+        });
+        if (correct) {
+            next();
+        } else {
+            next(new HttpError('Some required fields are not filled in', 400, { missing: missing }))
+        }
     }
-  }
 }
 
 const hasBody = () => {
-  return (req, res, next) => {
-    if (!req.body) next(new HttpError('No information given', 400))
-  }
+    return (req, res, next) => {
+        if (Object.keys(req.body).length === 0) {
+            next(new HttpError('No information given', 400))
+        } else {
+            next();
+        }
+    }
 }
 
 const login = ['username', 'password'];
 const newUser = ['username', 'password', 'confirmPassword', 'email'];
 
 module.exports = {
-  check,
-  hasBody,
-  login,
-  newUser,
+    check,
+    hasBody,
+    login,
+    newUser,
 }
