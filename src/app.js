fe9451ff91e2d27af07e77c8d0b7992ead8ece01
Benoit Welsch <bwelsch@helb-prigogine.be>
2021-03-09 11:01:37 +0100

    Body-Parser removed & comment added :  - BodyParser is deprecated and has been replace by express.json  - Comments have been hadded to improve code quality

--- src/app.js
+++ src/app.js
@@ -1,31 +1,33 @@
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const { isConnected, tokenParser, role } = require('./middleware/auth');
-
 const { InvalidUrl } = require("./error/httpError");
 
+//middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(tokenParser());
-app.use
 
+//require router
 const router = (router) => {
     return require('./router/' + router)
 };
 
+//route
 app.use('/auth', router('auth'));
 app.use('/user', isConnected(), router('user'));
 app.use('/item', router('item'));
 app.use('/provider', isConnected(), role(["Admin", "Worker"]), router('provider'));
 app.use('/config', router('config'));
 
+//default route 
 app.all('/*', () => {
     throw new InvalidUrl();
 });
 
+//error route
 app.use(require('./middleWare/error')())
 
 app.listen(process.env.PORT || 3000);
