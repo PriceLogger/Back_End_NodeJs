259eec0041f1441566597ca2cd23537d9abafb3d
Benoit Welsch <bwelsch@helb-prigogine.be>
2021-03-09 11:02:57 +0100

    Role added to improve security :  - Worker can only acces the 'GET' provider route

--- src/router/provider.js
+++ src/router/provider.js
@@ -1,27 +1,27 @@
 const router = require('express').Router();
 const Provider = require('../models/').Provider;
-const {check} = require('../middleware/validator');
-const {auth, role} = require('../middleware/auth');
+const { check } = require('../middleware/validator');
+const { auth, role } = require('../middleware/auth');
 
-router.get('/', async ({res}) => {
-  res.json(await Provider.findAll());
+router.get('/', async({ res }) => {
+    res.json(await Provider.findAll());
 });
 
-router.post('/', check(['name', 'priceTag', 'nameTag']), async (req, res) => {
-  res.json(await Provider.create(req.body));
+router.post('/', role('Admin'), check(['name', 'priceTag', 'nameTag']), async(req, res) => {
+    res.json(await Provider.create(req.body));
 });
 
-router.patch('/:id(\\d+)/', async (req, res) => {
-  res.json(Provider.findByPk(req.params.id).then((provider) => {
-    let updatedProvider = {...provider, ...req.body, ...{id: req.params.id}};
-    updatedProvider.save();
-  }))
+router.patch('/:id(\\d+)/', role('Admin'), async(req, res) => {
+    res.json(Provider.findByPk(req.params.id).then((provider) => {
+        let updatedProvider = {...provider, ...req.body, ... { id: req.params.id } };
+        updatedProvider.save();
+    }))
 });
 
-router.delete('/:id(\\d+)/', async (req, res) => {
-  res.json(await Provider.findByPk(req.params.id).then((provider) => {
-    provider.destroy();
-  }))
+router.delete('/:id(\\d+)/', role('Admin'), async(req, res) => {
+    res.json(await Provider.findByPk(req.params.id).then((provider) => {
+        provider.destroy();
+    }))
 });
 
 module.exports = router;
